ef49ac756aa35282000aa3a5ee6ac17d6c85594b
Gracilin <gracygracilin1865@gmail.com>
2024-07-29 13:28:17 +0530

    first commit

---
+++ src/App.js
@@ -0,0 +1,36 @@
+// src/App.js
+import React, { useState } from 'react';
+import axios from 'axios';
+import './App.css';
+
+function App() {
+  const [name, setName] = useState('');
+  const [email, setEmail] = useState('');
+
+  const handleSubmit = async (e) => {
+    e.preventDefault();
+    const response = await axios.post('http://localhost:5000/api/user', { name, email });
+    console.log(response.data);
+  };
+
+  return (
+    <div className="App">
+      <h1>Simple User Form</h1>
+      <form onSubmit={handleSubmit}>
+        <label>
+          Name:
+          <input type="text" value={name} onChange={(e) => setName(e.target.value)} />
+        </label>
+        <br />
+        <label>
+          Email:
+          <input type="email" value={email} onChange={(e) => setEmail(e.target.value)} />
+        </label>
+        <br />
+        <button type="submit">Submit</button>
+      </form>
+    </div>
+  );
+}
+
+export default App;
