f3947dea1b358abf2a1ffa2d3428d7940c5119e7
Gracilin <gracygracilin1865@gmail.com>
2024-07-31 16:01:33 +0530

    Update App.js

--- src/App.js
+++ src/App.js
@@ -27,7 +27,7 @@ function App() {
           <input type="email" value={email} onChange={(e) => setEmail(e.target.value)} />
         </label>
         <br />
-        <button type="submit">Submit</button>
+        <button type="submit">login</button>
       </form>
     </div>
   );
